7cb9defa870feeeb8b691e70408332fab60c4a5e
KocembaPiotr <KocembaPiotr@gmail.com>
2023-09-11 10:41:40 +0200

    Deploy

--- pkoffice/sql.py
+++ pkoffice/sql.py
@@ -224,10 +224,58 @@ def columns_str_max_len(df: pd.DataFrame) -> None:
         print(column)
 
 
-def parse_date_from_number(df: pd.Series) -> pd.Series:
+def parse_to_date_from_number(df: pd.DataFrame,  column_names: list) -> pd.DataFrame:
     """
-    Function to parse date from ordinal numbers for single series.
-    :param df: series with only date columns
-    :return: series with converted column
+    Function to parse date from ordinal numbers
+    :param df: pandas dataframe with data to convert
+    :param column_names: list of columns which need to be converted
+    :return: pandas dataframe with corrected types
     """
-    return df.apply(lambda x: datetime.fromordinal(datetime(1900, 1, 1).toordinal() + int(x) - 2))
+    for column_name in column_names:
+        df[column_name] = df[column_name].apply(
+            lambda x: datetime.fromordinal(datetime(1900, 1, 1).toordinal() + int(x) - 2))
+    return df
+
+
+def parse_to_date_from_str(df: pd.DataFrame,  column_names: list,
+                           format_from: str = '%d.%m.%Y',
+                           format_to: str = '%Y-%m-%d') -> pd.DataFrame:
+    """
+    Funtion to parse date from string
+    :param df: pandas dataframe with data to convert
+    :param column_names: list of columns which need to be converted
+    :param format_from: date format in string
+    :param format_to: desired date format
+    :return: pandas dataframe with corrected types
+    """
+    for column_name in column_names:
+        df[column_name] = pd.to_datetime(df[column_name], errors='coerce',
+                                         format=format_from).dt.strftime(format_to)
+    return df
+
+
+def parse_to_float(df: pd.DataFrame, column_names: list) -> pd.DataFrame:
+    """
+    Function to convert data to float type
+    :param df: pandas dataframe with data to convert
+    :param column_names: list of columns which need to be converted
+    :return: pandas dataframe with corrected types
+    """
+    for column_name in column_names:
+        df[column_name] = df[column_name].apply(lambda x: float(re.sub(',', '.', re.sub(' ', '', x))))
+    return df
+
+
+def parse_to_int_with_str_nan(df: pd.DataFrame, column_names: list) -> pd.DataFrame:
+    """
+    Function to convert data to int type with nulls
+    :param df: pandas dataframe with data to convert
+    :param column_names: list of columns which need to be converted
+    :return: pandas dataframe with corrected types
+    """
+    for column_name in column_names:
+        df[column_name] = df[column_name].fillna(-9999)
+        df[column_name] = df[column_name].astype(int)
+        df[column_name] = df[column_name].astype(str)
+        df[column_name] = df[column_name].replace('-9999', np.nan)
+    return df

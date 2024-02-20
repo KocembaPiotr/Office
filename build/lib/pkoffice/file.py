457656de697b19039a408af4bf70f5beef9fd1f2
KocembaPiotr <KocembaPiotr@gmail.com>
2024-02-20 10:07:13 +0100

    Deploy

--- build/lib/pkoffice/file.py
+++ build/lib/pkoffice/file.py
@@ -182,3 +182,36 @@ def folder_copy(dir_source_path: str, dir_destination_path: str) -> None:
 def folder_create(folder_path: str) -> None:
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
+
+
+def version_read(file_path: str, version_pattern: str) -> str:
+    """
+    :param file_path: path to file where version is put
+    :param version_pattern: pattern to read version number
+    :return: version statement
+    """
+    try:
+        with open(file_path, 'r') as f:
+            first_line = f.readline()
+            return re.sub(version_pattern, '', first_line)
+    except Exception as e:
+        print(e)
+        return ''
+
+
+def version_update(file_local_path: str, file_server_path: str,
+                   version_pattern: str) -> None:
+    """
+    Method to update version to new one.
+    :param file_local_path: path to file on local computer
+    :param file_server_path: path to file on server repository
+    :param version_pattern: pattern to extract proper version number
+    :return: None
+    """
+    if file_exists(file_local_path):
+        local_functions_version = version_read(file_local_path, version_pattern)
+        server_functions_version = version_read(file_server_path, version_pattern)
+        if local_functions_version != server_functions_version:
+            file_copy(file_server_path, file_local_path)
+    else:
+        file_copy(file_server_path, file_local_path)

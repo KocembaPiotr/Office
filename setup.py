0db45fd586c1a4dd45233fbade8023dbfb084dfd
KocembaPiotr <KocembaPiotr@gmail.com>
2024-02-23 14:03:32 +0100

    Deploy

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.70'
+VERSION = '0.0.71'
 DESCRIPTION = 'Python package to manage office tools.'
 LONG_DESCRIPTION = '''Python package which will simplify usage of main office tools.
                    '''
